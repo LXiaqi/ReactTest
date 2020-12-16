057d7cff0afd5fe6ffce6914ba6f156584783c32
LXiaqi <18534799988@163.com>
2020-12-17 01:38:04 +0800

    快递单查询进度条

--- src/pages/searchExpress/searchExpress.js
+++ src/pages/searchExpress/searchExpress.js
@@ -8,7 +8,13 @@ class searchExpress extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchNum: ''
+            searchNum: '',
+            length:0,
+            expresslogo:'',
+            expressname:'',
+            expresstype:'',
+            expressltime:'',
+            expressnum:''
         }
     };
     // 输入框的change 事件， 获取val
@@ -20,7 +26,38 @@ class searchExpress extends React.Component {
     // 搜索按钮，发送请求改变数据
     SearchExpress() {
         express(this).then(res => {
-            console.log(res);
+        console.log(res.data.result.list);
+        // let switchtype =()=> {
+        //     switch (res.data.result.deliverystatus) {
+        //         case 1:
+        //           return <h4>派送中</h4>;
+        //           break;
+        //         case 0:
+        //           return <h4>已签收</h4>;
+        //           break;
+        //         default:
+        //           return null;
+        //       }
+        // }
+        this.setState({
+            expresslogo:res.data.result.logo,
+            expressname:res.data.result.expName,
+            // expresstype:switchtype,
+            expressltime:res.data.result.updateTime,
+            expressnum:res.data.result.number
+        })
+          this.StepsList(res.data.result.list);
+        })
+    };
+    // 步骤条数据展示
+    StepsList = (data) => {
+       const stepsResult = data.map((item,index)=> {
+            return (
+                <Step title={item.time} description={item.status}  key={index} />
+            );
+        })
+        this.setState({
+            stepsResult,
         })
     };
     render() {
@@ -32,16 +69,19 @@ class searchExpress extends React.Component {
                     <Button className="btn_search" type="primary" onClick={() => this.SearchExpress()}>搜索</Button>
                 </Card>
                 <Card className='result_content'>
-                    
+                    <div>
+                        <div><img src={this.state.expresslogo} alt="logo"/></div>
+                        <div>
+                           
+                            <span>{this.state.expressname}</span>
+                            <span>快递单号：{this.state.expressnum}</span>
+                            <span>更新时间：{this.state.expressltime}</span>
+                        </div>
+                    </div>
                     <Divider />
-                    <Steps progressDot current={1} direction="vertical">
-                        <Step title="Finished" description="This is a description. This is a description." />
-                        <Step title="Finished" description="This is a description. This is a description." />
-                        <Step title="In Progress" description="This is a description. This is a description." />
-                        <Step title="Waiting" description="This is a description." />
-                        <Step title="Waiting" description="This is a description." />
+                    <Steps progressDot current={this.state.length} direction="vertical">
+                       {this.state.stepsResult}
                     </Steps>
-
                 </Card>
             </div>
         )
